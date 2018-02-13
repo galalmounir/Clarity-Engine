309e2aefe59db3b1e15998989b5ef817f0a14433
Jee <galal@thepixelcrushers.com>
2018-02-12 21:22:24 -0500

    Initial Commit

---
+++ Clarity Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clarity Engine
+//
+//  Created by Galal Hassan on 2018-02-12.
+//  Copyright © 2018 The Pixel Crushers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
